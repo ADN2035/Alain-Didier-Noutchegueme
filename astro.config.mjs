9ffa7eeeb7bff733417d75ff24394abd2188a1e0
Noutchegueme Alain Didier <alaindidier@MacBook-Air-de-Noutchegueme.local>
2026-06-06 08:44:17 -0400

    fix: restauration de l'intégration Tailwind CSS dans la configuration Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://adn2035.github.io',
+  site: 'https://ton-pseudo.github.io',
   base: '/infrastructure-web',
   integrations: [
     tailwind(),
